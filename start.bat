939c9ed34f9322e65185332ad016bed491f2236f
abudgetbarber <abudgetbarber@gmail.com>
2026-04-10 13:46:59 +0530

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing Budget Barber dependencies...
-pip install -r requirements.txt
-echo.
-echo Starting Budget Barber server...
-echo Open http://localhost:5000 in your browser
-echo.
-python server.py
-pause
