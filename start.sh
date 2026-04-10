0e700b4add52298d9ec808fecbb4ac9cf45f996f
abudgetbarber <abudgetbarber@gmail.com>
2026-04-10 13:47:09 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing Budget Barber dependencies..."
-pip install -r requirements.txt
-echo ""
-echo "Starting Budget Barber server..."
-echo "Open http://localhost:5000 in your browser"
-echo ""
-python server.py
